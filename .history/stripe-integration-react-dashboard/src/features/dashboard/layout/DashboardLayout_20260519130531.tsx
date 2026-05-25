2b5d811a1d621f80379c114a5d5f0c42e6eb2af1
Farazdaq <sleekdxb@gmail.com>
2026-05-25 13:59:09 +0400

    full redme file and important descrption

---
+++ .history/stripe-integration-react-dashboard/src/features/dashboard/layout/DashboardLayout_20260519130531.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { useTheme } from "../../../theme/useTheme";
+
+import {
+  Navbar,
+  Sidebar,
+  Overview,
+  Customer,
+  Package,
+  Invoice,
+  Transaction,
+  Subscription,
+} from "./export.ts";
+
+export default function DashboardLayout() {
+  const { theme } = useTheme();
+  const [activePage, setActivePage] = useState("overview");
+
+  return (
+    <div
+      className="flex flex-col w-screen h-screen overflow-hidden"
+      style={{
+        backgroundColor: theme.colors.bg,
+      }}
+    >
+      {/* TOP NAVBAR */}
+      <div className="shrink-0">
+        <Navbar />
+      </div>
+
+      {/* BODY */}
+      <div className="flex flex-1 min-h-0 overflow-hidden">
+        {/* SIDEBAR */}
+        <div className="h-full shrink-0">
+          <Sidebar active={activePage} setActive={setActivePage} />
+        </div>
+
+        {/* MAIN CONTENT */}
+        <main className="flex-1 h-full overflow-y-auto overflow-x-hidden pt-1 pl-1">
+          <div className="min-h-full">
+           
+            {activePage === "customer" && <Customer />}
+            {activePage === "package" && <Package />}
+           
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ .history/stripe-integration-react-dashboard/src/features/dashboard/layout/Sidebar/sidebar.config_20260519131402.ts
@@ -0,0 +1,22 @@
+import {
+  FaChartPie,
+  FaUsers,
+  FaBoxOpen,
+  FaFileInvoice,
+  FaExchangeAlt,
+  FaSyncAlt,
+} from "react-icons/fa";
+
+export const sidebarItems = [
+  {
+    key: "customer",
+    label: "Customer",
+    icon: FaUsers,
+  },
+
+  {
+    key: "package",
+    label: "Package",
+    icon: FaBoxOpen,
+  },
+];
